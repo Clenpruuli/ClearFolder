b78bdc572445f351d8a93c4df46beeb1051044ec
Opilane TTHK <opilane@tthk.ee>
2020-11-19 13:00:36 +0200

    first commit

---
+++ CreateFile/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace CreateFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the file name include extension: ");
+            string userInput = Console.ReadLine();
+            CreateFileWithExtension(userInput);
+        }
+
+        public static void CreateNewFile()
+        {
+            string rootPath = @"C:\Users\opilane\samples\playerOne";
+            string fileName = "ball.txt";
+            File.Create(Path.Combine(rootPath, fileName));
+        }
+
+        public static void CreateUserFile(string fileName)
+        {
+            string rootPath = @"C:\Users\opilane\samples\playerTwo";
+            File.Create(Path.Combine(rootPath, fileName));
+        }
+
+        public static void CreateFileWithExtension(string fileName)
+        {
+            string rootPath = @"C:\Users\opilane\samples\playerTwo";
+            string fullfileName = $"{fileName}.txt";
+            File.Create(Path.Combine(rootPath, fullfileName));
+        }
+    }
+}
